cbdb4f4af7329672f5e8dca6c08e1e6eb67d26ec
Kyczar Aalbregtse <kyczaraalbregtse@gmail.com>
2024-08-09 15:43:30 -0400

    structural and styling work on footer and recommended recipes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import '../App.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+library.add( faSquareGithub, faLinkedin );
+
 export default function Footer () {
     return (
         <div className="wte-footer">
-            Site made by Kyczar Aalbregtse
+            <p>Site made by Kyczar Aalbregtse</p>
+            <div>
+                <a href="https://github.com/kaalbregtse" target="_blank" rel="noreferrer"><FontAwesomeIcon icon="fa-brands fa-square-github" /></a>
+                <a href="https://www.linkedin.com/in/kyczar-aalbregtse-52b545199/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon="fa-brands fa-linkedin" /></a>
+            </div>
         </div>
     )
 }
--- src/components/recommended.jsx
+++ src/components/recommended.jsx
@@ -1,9 +1,59 @@
 import React from "react";
+import Food from '../food.jpeg';
 
 export default function Recommended () {
+
+    /*const url = 'https://api.spoonacular.com/recipes/random?apiKey=a3670b26718c422c82c57389e3ab404b&instructionsRequired=True&number=3';
+
+    var request = new XMLHttpRequest();
+    request.open("GET", url, false);
+    request.send(null);
+    var recipesObj = JSON.parse(request.responseText);
+
+    console.log('hello')
+
+    fetch(url, {
+        method: 'GET'
+    }).then((response) => response.json());*/
+
+
     return (
-        <div>
+        <section className="wte-recommended-meals-container">
             <h2>Recommended Meals of the Day</h2>
-        </div>
+
+            <div className="wte-recipe-card-container">
+                <div className='wte-recipe-card' style={{backgroundImage: `url(${Food})`}}>
+                    <div className="wte-recipe-info">
+                        <h3>Burger</h3>
+                        <div className="wte-recipe-tags">
+                            <p>Vegetarian</p>
+                            <p>Simple</p>
+                            <p>Dinner</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='wte-recipe-card' style={{backgroundImage: `url(${Food})`}}>
+                    <div className="wte-recipe-info">
+                        <h3>Burger</h3>
+                        <div className="wte-recipe-tags">
+                            <p>Breakfast</p>
+                            <p>Sweet</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='wte-recipe-card' style={{backgroundImage: `url(${Food})`}}>
+                    <div className="wte-recipe-info">
+                        <h3>Burger</h3>
+                        <div className="wte-recipe-tags">
+                            <p>Tags</p>
+                            <p>Tags</p>
+                            <p>Tags</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
